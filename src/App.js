e913d5ac1caf56acfb21420c474d60fd937c6058
Yuka Yeh <yukaka210@gmail.com>
2021-07-07 15:06:24 +0200

    route pin drop planning done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react'
 import * as tt from '@tomtom-international/web-sdk-maps'
-import './App.css';
-import '@tomtom-international/web-sdk-maps/dist/maps.css';
+import * as ttapi from '@tomtom-international/web-sdk-services'
+import './App.css'
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
 
 const App = () => {
-
   const mapElement = useRef()
-  const [ map, setMap] = useState({})
-  const [ longitude, setLongitude ] = useState(-0.112869)
-  const [ latitude, setLatitude ] = useState(51.504)
+  const [map, setMap] = useState({})
+  const [longitude, setLongitude] = useState(4.899431)
+  const [latitude, setLatitude] = useState(52.379189)
+
+  const convertToPoints = (lngLat) => {
+    return {
+      point: {
+        latitude: lngLat.lat,
+        longitude: lngLat.lng
+      }
+    }
+  }
+
+  const addDeliveryMarker = (lngLat, map) => {
+    const element = document.createElement('div')
+    element.className = 'marker-delivery'
+    new tt.Marker({
+      element: element
+    })
+    .setLngLat(lngLat)
+    .addTo(map)
+  }
+
+  const drawRoute = (geoJson, map) => {
+    if (map.getLayer('route')) {
+      map.removeLayer('route')
+      map.removeSource('route')
+    }
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: geoJson
+      },
+      paint: {
+        'line-color': '#4a90e2',
+        'line-width': 6
+  
+      }
+    })
+  }
 
   useEffect(() => {
+    const origin = {
+      lng: longitude,
+      lat: latitude,
+    }
+    const destinations = []
 
     let map = tt.map({
       key: process.env.REACT_APP_TOM_TOM_API_KEY,
@@ -20,28 +64,26 @@ const App = () => {
         trafficFlow: true,
       },
       center: [longitude, latitude],
-      zoom: 14
-    });
+      zoom: 14,
+    })
 
-    setMap(map);
+    setMap(map)
 
     const addMarker = () => {
-
       const popupOffset = {
         bottom: [0, -25]
       }
-      const popup = new tt.Popup({ offset: popupOffset }).setHTML('This is you!')
-
-      const element = document.createElement('div');
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML('You\'re here')
+      const element = document.createElement('div')
       element.className = 'marker'
 
       const marker = new tt.Marker({
         draggable: true,
         element: element,
       })
-      .setLngLat([longitude, latitude])
-      .addTo(map);
-
+        .setLngLat([longitude, latitude])
+        .addTo(map)
+      
       marker.on('dragend', () => {
         const lngLat = marker.getLngLat()
         setLongitude(lngLat.lng)
@@ -49,34 +91,98 @@ const App = () => {
       })
 
       marker.setPopup(popup).togglePopup()
+      
     }
+    addMarker()
 
-    addMarker();
+    const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination)
+      })
+      const callParameters = {
+        key: process.env.REACT_APP_TOM_TOM_API_KEY,
+        destinations: pointsForDestinations,
+        origins: [convertToPoints(origin)],
+      }
 
     return new Promise((resolve, reject) => {
-      
+      ttapi.services
+        .matrixRouting(callParameters)
+        .then((matrixAPIResults) => {
+          const results = matrixAPIResults.matrix[0]
+          const resultsArray = results.map((result, index) => {
+            return {
+              location: locations[index],
+              drivingtime: result.response.routeSummary.travelTimeInSeconds,
+            }
+          })
+          resultsArray.sort((a, b) => {
+            return a.drivingtime - b.drivingtime
+          })
+          const sortedLocations = resultsArray.map((result) => {
+            return result.location
+          })
+          resolve(sortedLocations)
+        })
+      })
+    }
+
+    const recalculateRoutes = () => {
+      sortDestinations(destinations).then((sorted) => {
+        sorted.unshift(origin)
+
+        ttapi.services
+          .calculateRoute({
+            key: process.env.REACT_APP_TOM_TOM_API_KEY,
+            locations: sorted,
+          })
+          .then((routeData) => {
+            const geoJson = routeData.toGeoJson()
+            drawRoute(geoJson, map)
+        })
+      })
+    }
+
+
+    map.on('click', (e) => {
+      destinations.push(e.lngLat)
+      addDeliveryMarker(e.lngLat, map)
+      recalculateRoutes()
     })
 
-    return () => map.remove();
-
+    return () => map.remove()
   }, [longitude, latitude])
 
   return (
     <>
-    { map && <div className="app">
-     <div ref={mapElement} className="map" />
-     <div className="search-bar">
-       <h1>Where to?</h1>
-       <input type='text' 
-              id='longitude' className='longitude' placeholder='Put in longitude' 
-              onChange={ (e) =>{setLongitude(e.target.value)}}></input>
-       <input type='text' 
-              id='latitude' className='latitude' placeholder='Put in latitude' 
-              onChange={ (e) =>{setLatitude(e.target.value)}}></input>
-     </div>
-    </div>}
+      {map && (
+        <div className="app">
+          <div ref={mapElement} className="map" />
+          <div className="search-bar">
+            <h1>Which City?</h1>
+            <input
+              type="text"
+              id="longitude"
+              className="longitude"
+              placeholder="Put in Longitude"
+              onChange={(e) => {
+                setLongitude(e.target.value)
+              }}
+            />
+            <input
+              type="text"
+              id="latitude"
+              className="latitude"
+              placeholder="Put in Latitude"
+              onChange={(e) => {
+                setLatitude(e.target.value)
+              }}
+            />
+          </div>
+        </div>
+      )}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
